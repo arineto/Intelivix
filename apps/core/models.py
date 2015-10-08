26ca4b3ab55ec72c88aed6a9b451be3e11b0d360
Arimatea Neto <arineto30@gmail.com>
2015-10-07 23:18:28 -0300

    Criacao dos modelos mais as telas de listagem das pessoas e do log

---
+++ apps/core/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Person(models.Model):
+
+	code = models.IntegerField(verbose_name="Código")
+	name = models.CharField(max_length=150, verbose_name="Nome")
+
+	def __str__(self):
+		return "%s (%d)" % (self.name, self.code)
+
+	class Meta:
+		verbose_name = 'Pessoa'
+		verbose_name_plural = 'Pessoas'
+
+
+class Log(models.Model):
+
+	CREATE = 0
+	UPDATE = 1
+	DELETE = 2
+	DETAIL = 3
+
+	ACTION_TYPE = (
+			(CREATE, 'Create'),
+			(UPDATE, 'Update'),
+			(DELETE, 'Delete'),
+			(DETAIL, 'Detail'),
+		)
+
+	# Decided to use a String instead of a Foreign Key to Person because if we use the Foreign Key the data will be lost once the instace is deleted
+	person = models.CharField(max_length=100, verbose_name='Pessoa')
+	action_type = models.IntegerField(choices=ACTION_TYPE, verbose_name='Tipo da Ação')
+	date = models.DateTimeField(auto_now_add=True, verbose_name='Data')
+
+	def __str__(self):
+		return "%s %s - %s" % (self.get_action_type_display(), self.person, self.date.strftime('%d/%m/%Y %H:%M'))
---
+++ apps/core/views.py
@@ -0,0 +1,19 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Person, Log
+
+
+class PersonListView(ListView):
+	# fazer mixin para adicionar uma lista como os nomes já existentes para ao autocomplete
+	model = Person
+	context_object_name = 'person_list'
+	template_name = 'person_list'
+
+
+class LogListView(ListView):
+	# fazer mixin para adicionar uma lista como os nomes já existentes para ao autocomplete
+	model = Log
+	context_object_name = 'log_list'
+	template_name = 'log_list'
